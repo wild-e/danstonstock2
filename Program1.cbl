905ba955cf95c73f94b09b0098c5413e5babeb5f
wild-e <wild.emmanuelle@gmail.com>
2022-12-01 11:56:20 +0100

    feat: insert command

--- Program1.cbl
+++ Program1.cbl
@@ -1,7 +1,32 @@
 
        program-id. Program1 as "danstonstock.danstonstock".
 
+       environment division.
+       input-output section.
+       file-control.
+           select f-fichierCommande1 assign to "E:\Emma\DUGS\fichierCommande1.csv"
+           organization is line sequential.
+           select f-fichierCommande2 assign to "E:\Emma\DUGS\fichierCommande2.csv"
+           organization is line sequential.
+           select f-fichierCommande3 assign to "E:\Emma\DUGS\fichierCommande3.csv"
+           organization is line sequential.
+           select f-fichierCommande4 assign to "E:\Emma\DUGS\fichierCommande4.csv"
+           organization is line sequential.
+
+
+
        data division.
+
+       file section.
+       fd f-fichierCommande1 record varying from 0 to 255.
+       01 e-fichierCommande1 pic x(255).
+       fd f-fichierCommande2 record varying from 0 to 255.
+       01 e-fichierCommande2 pic x(255).
+       fd f-fichierCommande3 record varying from 0 to 255.
+       01 e-fichierCommande3 pic x(255).
+       fd f-fichierCommande4 record varying from 0 to 255.
+       01 e-fichierCommande4 pic x(255).
+
        working-storage section.
 
       ************************************************************
@@ -22,6 +47,8 @@
               
        77 ChoixMenuPrincipal PIC X.
        77 ChoixMenuArticle PIC X.
+       77 ChoixMenuCommande PIC X.
+       77 ChoixNoCommande pic X.
 
        01 DateSysteme.
          10 Jour PIC 99.
@@ -37,11 +64,26 @@
          10 date_crea sql date.
          10 date_modif sql date.
 
-       77 NoLigneArticle pic 99.
+       01 Commande.
+           10 code_fournisseur sql char (10).
+           10 no_commande sql char (10).
+           10 date_commande pic x(8).
+           10 code_article pic 9(10).
+           10 quantite pic 9(10).
+
+       77 CodeFournisseur pic x(9).
+       77 QuantiteTotalCommande pic 9(6).
+       77 NoLigneArticle pic 9(10).
+       77 NoLigneCommande pic 9(10).
+       77 TotalLigneCommande pic 9(10).
        77 Choix pic x.
        77 Response pic x.
        77 EOF pic 9.
+       77 EOR pic 9.
        77 EOA pic 9.
+       77 tally-counter pic 9.
+
+       01 MessageErreurCommande pic x(80).
 
       ************************************************************
       * Paramétrage couleur écran
@@ -61,7 +103,8 @@
          10 line 5 col 9 from Mois of DateSysteme.
          10 line 5 col 12 value "/".
          10 line 5 col 14 from Annee of DateSysteme.
-         10 line 5 col 68 VALUE "Choix:".
+         10 line 5 col 68 VALUE "Choix: ".
+         10 line 5 col 77 pic 9 from ChoixMenuArticle.
          10 line 11 col 15 VALUE "1. Gestions des articles".
          10 line 12 col 15 VALUE "2. Gestion des fournisseurs".
          10 line 13 col 15 VALUE "3. Reception commande".
@@ -85,6 +128,29 @@
          10 line 14 col 15 value "4. Supprimer un article".
          10 line 15 col 15 value "5. Consulter - Stock".
          10 line 17 col 15 value "0. Retour au menu principal".
+
+      ********** MENU commande ******
+
+       01 ecran-MenuCommande background-color is CouleurFondEcran foreground-color is CouleurCaractere.
+         10 line 1 col 1 Blank Screen.
+         10 line 3 col 32 value " Ajout des commandes ".
+         10 line 5 col 68 value "Choix: ".
+         10 line 5 col 77 pic 9 from Choix.
+         10 line 12 col 15 value "1. Commande A - total article non conforme".
+         10 line 13 col 15 value "2. Commande B - Mise en forme non conforme".
+         10 line 14 col 15 value "3. Commande C - fichier correct".
+         10 line 15 col 15 value "4. Commande D - fournisseur inconnu".
+         10 line 17 col 15 VALUE "0. Quitter".
+
+       01 ligne-MenuCommandeErreur background-color is CouleurFondEcran foreground-color is CouleurCaractere.
+         10 line 6 col 7 VALUE "Le programme s'est arrete sans modifier la base de donnee car:" reverse-video.
+         10 line 7 col 7 pic x(80) from MessageErreurCommande.
+         10 line 17 col 15 value "          ".
+
+       01 ligne-MenuCommandeSucces background-color is CouleurFondEcran foreground-color is CouleurCaractere.
+         10 line 6 col 7 VALUE "Commande ajoutee avec succes" reverse-video.
+         10 line 17 col 15 value "          ".
+
 
       ********** LISTE ARTICLE *****
 
@@ -130,7 +196,7 @@
            accept DateSysteme from date yyyymmdd.
       ******** Connection BDD
            move
-             "Trusted_Connection=yes;Database=Papillon;server=DESKTOP-16DLBER\SQLEXPRESS;factory=system.Data.SqlClient;"
+             "Trusted_Connection=yes;Database=danstonstock2;server=DESKTOP-16DLBER\SQLEXPRESS;factory=system.Data.SqlClient;"
              to CNXDB.
            exec sql
              connect using :CNXDB
@@ -154,6 +220,8 @@
            evaluate ChoixMenuPrincipal
                when 1
                    perform MenuArticle
+               when 3
+                   perform MenuCommande
            end-evaluate.
 
        MenuPrincipal-fin.
@@ -182,7 +250,7 @@
                    perform ListeArticle
                when 2
                    perform AjoutArticle
-
+               
            end-evaluate.
        MenuArticle-fin.
            stop run.
@@ -263,5 +331,328 @@
            continue.
        AjoutArticle-fin.
            continue.
+
+      *************************************************************
+      *************************************************************
+      * Gestion menu commande
+      *************************************************************
+      *************************************************************
+       MenuCommande.
+           perform MenuCommande-init.
+           perform MenuCommande-trt until ChoixMenuCommande equal 0.
+           perform MenuCommande-fin.
+
+       MenuCommande-init.
+           move 1 to ChoixMenuCommande.
+
+       MenuCommande-trt.
+           move 0 to ChoixMenuCommande.
+           move 0 to ChoixNoCommande
+           display ecran-MenuCommande.
+           accept ChoixNoCommande line 5 col 77.
+
+           evaluate ChoixNoCommande
+               when 0
+                   move 0 to ChoixMenuCommande
+               when other
+                   perform TraitementFichierCommande
+           end-evaluate.
+      
+       MenuCommande-fin.
+           continue.
+
+      *************************************************************
+      *************************************************************
+      * Evaluates correspondant au bon noms de fichiers
+      *************************************************************
+      *************************************************************
+       OpenInput.
+           evaluate ChoixNoCommande
+               when 1
+                   open input f-fichierCommande1
+               when 2
+                   open input f-fichierCommande2
+               when 3
+                   open input f-fichierCommande3
+               when 4
+                   open input f-fichierCommande4
+
+           end-evaluate.
+
+       ReadFichier.
+           evaluate ChoixNoCommande
+               when 1
+                   read f-fichierCommande1
+               when 2
+                   read f-fichierCommande2
+               when 3
+                   read f-fichierCommande3
+               when 4
+                   read f-fichierCommande4
+           end-evaluate.
+
+       ReadFichierToEnd.
+           evaluate ChoixNoCommande
+               when 1
+                   read f-fichierCommande1
+                       at end
+                           perform VerificationFichier-derniereLigne
+                       not at end
+                           perform VerificationFichier-corps
+                   end-read
+               when 2
+                   read f-fichierCommande2
+                       at end
+                           perform VerificationFichier-derniereLigne
+                       not at end
+                           perform VerificationFichier-corps
+                   end-read
+               when 3
+                   read f-fichierCommande3
+                       at end
+                           perform VerificationFichier-derniereLigne
+                       not at end
+                           perform VerificationFichier-corps
+                   end-read
+               when 4
+                   read f-fichierCommande4
+                       at end
+                           perform VerificationFichier-derniereLigne
+                       not at end
+                           perform VerificationFichier-corps
+                   end-read
+
+           end-evaluate.
+
+       UnstringCommandeEntete.
+           evaluate ChoixNoCommande
+               when 1
+                   unstring e-fichierCommande1 delimited by ","
+                     into
+                     code_fournisseur of Commande
+                     no_commande of Commande
+                     date_commande of Commande
+                   end-unstring
+               when 2
+                   unstring e-fichierCommande2 delimited by ","
+                     into
+                     code_fournisseur of Commande
+                     no_commande of Commande
+                     date_commande of Commande
+                   end-unstring
+               when 3
+                   unstring e-fichierCommande3 delimited by ","
+                     into
+                     code_fournisseur of Commande
+                     no_commande of Commande
+                     date_commande of Commande
+                   end-unstring
+               when 4
+                   unstring e-fichierCommande4 delimited by ","
+                     into
+                     code_fournisseur of Commande
+                     no_commande of Commande
+                     date_commande of Commande
+                   end-unstring
+           end-evaluate.
+
+
+       CloseInput.
+           evaluate ChoixNoCommande
+               when 1
+                   close f-fichierCommande1
+               when 2
+                   close f-fichierCommande2
+               when 3
+                   close f-fichierCommande3
+               when 4
+                   close f-fichierCommande4
+
+           end-evaluate.
+
+       UnstringEnregistrementCommande.
+           evaluate ChoixNoCommande
+               when 1
+                   unstring e-fichierCommande1 delimited by ","
+                     into
+                     code_article of Commande
+                     quantite of Commande
+                   end-unstring
+               when 2
+                   unstring e-fichierCommande2 delimited by ","
+                     into
+                     code_article of Commande
+                     quantite of Commande
+                   end-unstring
+               when 3
+                   unstring e-fichierCommande3 delimited by ","
+                     into
+                     code_article of Commande
+                     quantite of Commande
+                   end-unstring
+               when 4
+                   unstring e-fichierCommande4 delimited by ","
+                     into
+                     code_article of Commande
+                     quantite of Commande
+                   end-unstring
+
+           end-evaluate.
+
+      *************************************************************
+      *************************************************************
+      * Gestion lecture fichier commande
+      *************************************************************
+      *************************************************************
+
+       TraitementFichierCommande.
+           perform VerificationFichier.
+           if MessageErreurCommande equal spaces
+               perform TraitementCommande
+           else
+               perform SortieErreurCommande
+           end-if.
+
+       SortieErreurCommande.
+           display ligne-MenuCommandeErreur.
+           accept ChoixNoCommande line 5 col 77.
+
+       VerificationFichier.
+           perform VerificationFichier-init.
+           perform VerificationFichier-trt until EOR = 1.
+           perform VerificationFichier-fin.
+
+       VerificationFichier-init.
+           move space to MessageErreurCommande
+           move 0 to EOR.
+           move 0 to NoligneCommande.
+           move 0 to QuantiteTotalCommande.
+           perform openInput.
+           perform VerificationEntete.
+
+       VerificationEntete.
+           perform ReadFichier.
+
+      *    On vérifie que l'entête est conforme
+           perform UnstringCommandeEntete.
+           inspect date_commande of Commande tallying tally-counter for all '/'.
+
+           if (code_fournisseur of Commande equal low-value
+               or no_commande of Commande equal low-value
+               or date_commande of Commande equal low-value
+               or tally-counter not equal 2
+               )
+               move "Entete du fichier non conforme" to MessageErreurCommande
+               move 1 to EOR
+           end-if.
+
+      *    On vérifie que le fournisseur existe en BDD
+           move zero to CodeFournisseur.
+           exec sql
+             SELECT id_fournisseur INTO :CodeFournisseur FROM fournisseur WHERE id_fournisseur = :code_fournisseur 
+           end-exec.
+           if (CodeFournisseur equal zero)
+               move "Fournisseur inconnu" to MessageErreurCommande
+               move 1 to EOR
+           end-if.
+
+       VerificationFichier-trt.
+           perform ReadFichierToEnd.
+
+       VerificationFichier-corps.
+           add 1 to NoligneCommande.
+
+      *    On vérifie que la ligne article est conforme
+           perform UnstringEnregistrementCommande.
+
+           if (code_article of Commande equal low-value
+               or code_article of Commande equal zero
+               or quantite of Commande equal low-value
+               or quantite of Commande equal zero
+               )
+               move "Corps du fichier non conforme" to MessageErreurCommande
+               move 1 to EOR
+           end-if.
+           add quantite of Commande to QuantiteTotalCommande.
+
+       VerificationFichier-derniereLigne.
+           move 1 to EOR.
+      *    on enlève la dernière ligne
+           subtract 1 from NoligneCommande.
+           string NoligneCommande delimited by size into TotalLigneCommande.
+           subtract quantite of Commande from QuantiteTotalCommande.
+
+      *    vérification du total de la quantite et du nombre de lignes
+           if (QuantiteTotalCommande not equal quantite of Commande
+               or TotalLigneCommande not equal code_article of Commande)
+               move "Quantite d'articles ou nombre de lignes totales non conforme" to MessageErreurCommande
+           end-if.
+
+       VerificationFichier-fin.
+           perform CloseInput.
+
+       TraitementCommande.
+           perform TraitementCommande-init.
+           perform TraitementCommande-trt until NoligneCommande = TotalLigneCommande.
+           perform TraitementCommande-fin.
+
+       TraitementCommande-init.
+           move spaces to Article.
+           move 1 to NoligneCommande.
+           perform OpenInput.
+
+      *    On passe la première ligne
+           perform ReadFichier.
+      *    Insertion entête commande
+           exec sql
+               INSERT INTO Commande (date_commande, id_fournisseur)
+                   VALUES (
+                       CAST(:Commande.date_commande as date),
+                       :Commande.code_fournisseur
+                       )
+           end-exec.
+      *    On récupère l'id de la commande nouvellement insérée
+           exec sql
+               SELECT scope_identity() into :Commande.no_commande
+           end-exec.
+
+       TraitementCommande-trt.
+           perform ReadFichier.
+           perform unstringEnregistrementCommande.
+           move zero to code_article of Article.
+           exec sql
+               select
+                   code_article,
+                   quantite_stock,
+                   quantite_min
+                   into
+                   :article.code_article,
+                   :article.quantite_stock,
+                   :article.quantite_min
+                   from article
+                   WHERE code_article = :commande.code_article AND id_fournisseur = :CodeFournisseur
+           end-exec.
+      *    MAJ quantite et stock article
+           add quantite of commande to quantite_stock of article.
+           exec sql
+               UPDATE article
+                   SET quantite_stock = :article.quantite_stock,
+                       date_modif = getdate()
+                       WHERE code_article = :commande.code_article AND id_fournisseur = :CodeFournisseur
+           end-exec.
+      *    Insertion commande article
+           exec sql
+               INSERT INTO ligne_commande (id_commande, code_article, quantite)
+                   VALUES (
+                       :commande.no_commande,
+                       :article.code_article,
+                       :commande.quantite
+                       )
+           end-exec.
+           add 1 to NoLigneCommande.
+       TraitementCommande-fin.
+           perform CloseInput.
+           display ligne-MenuCommandeSucces.
+           accept ChoixNoCommande line 5 col 77.
 
        end program Program1.
